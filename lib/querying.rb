9ea6e8260fcacc6ffc0b18488c9b16f84b71fd6f
Elijah wines <elijahwines84@yahoo.com>
2020-04-10 18:13:42 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -1,6 +1,6 @@
 def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.titles, books.years FROM books
-  JOIN series ON books.series_id = series.id 
+  JOIN series ON books.series_id = series.id
   WHERE series.id = 1
   ORDER BY books.year"
 end
